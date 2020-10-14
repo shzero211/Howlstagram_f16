11f5f8846344a8251943855b2d6eb8f804bbff01
SangHun <shzero211@naver.com>
2020-10-14 17:03:07 +0900

    10/14 commit

--- app/src/main/java/com/example/howlstagram_f16/MainActivity.kt
+++ app/src/main/java/com/example/howlstagram_f16/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.howlstagram_f16
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.howlstagram_f16.navigation.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),BottomNavigationView.OnNavigationItemSelectedListener{
+
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        TODO("Not yet implemented")
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        bottom_navigation.setOnNavigationItemReselectedListener(this)
+
+        bottom_navigation.setOnNavigationItemSelectedListener { it ->
+            when (it.itemId) {
+                R.id.action_home -> {
+                    val detailViewFragment = DetailViewFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_content, detailViewFragment).commit()
+                    true
+                }
+                R.id.action_search -> {
+                    val gridFragment = GridFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_content, gridFragment).commit()
+                    true
+                }
+                R.id.action_add_photo -> {
+                    if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){
+
+                        startActivity(Intent(this,AddPhotoActivity::class.java))
+                    }
+                    true
+                }
+                R.id.action_favorite_alarm -> {
+                    val alarmFragment = AlarmFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_content, alarmFragment).commit()
+                    true
+                }
+                R.id.action_account -> {
+                    val userFragment = UserFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_content, userFragment).commit()
+                    true
+                }
+                else -> false
+            }
+        }
+        ActivityCompat.requestPermissions(this,arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
     }
+
 }
